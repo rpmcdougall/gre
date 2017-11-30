1b0b5e22642f4dee9b39188acb9f0043b46f5b98
Ryan McDougall <rp.mcdougall@gmail.com>
2017-11-29 18:20:53 -0600

    Added mounting of psdrive from mount point input with error checking and added line to fetch file list into variable

--- psfileutils.psm1
+++ psfileutils.psm1
@@ -2,11 +2,25 @@ function Get-FileListJSON {
 param(
    [string]$mountpoint
 {
+    #Connect mount point as PSDrive to work with local and network mounts (SMB)
     #Fetch directory listing into variable use error checking if mount is unavailable or invalid
-
+    #Get-Childitem defaults to bytes
+    Try{
+        $psDrive = New-PSDrive -Name tempInspect -PSProvider FileSystem -Root $mountPoint -Description "Temporary mounting path for PSFileUtils Get-FileListJSON"
+    }
+    Catch{
+        Write-Host "Failed to add mountpoint as temporary PSDrive. Mount point is invalid or unavailable. Terminating script..."
+        exit
+    }
+    
+    $fileList = Get-ChildItem tempInspect: -recurse 
+    
+    
     #For each item in mount point, create custom psobjects containing properties
     #for the filepath and file size in bytes and accumulate them into array
 
     #Serialize and write out json
+
+    #Cleanup PSDrive
 
 }
